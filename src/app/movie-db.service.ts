6d7e1ddf5fb3f3abd2da737f2bdc292b4c47d660
Enrique de Miguel <Kiezmi@gmail.com>
2018-08-29 13:45:13 +0200

    servicio series

--- src/app/movie-db.service.ts
+++ src/app/movie-db.service.ts
@@ -17,7 +17,9 @@ export class MovieDbService {
   public getSeries():Observable<any>{
     return this.http.get<any>('https://api.themoviedb.org/3/tv/popular?api_key=81aca90227197885e3861bf1d2e1acd1&language=es-ES')
   }
-
+  public getSerie(id:number):Observable<any>{
+    return this.http.get<any>('https://api.themoviedb.org/3/tv/'+id+'?api_key=81aca90227197885e3861bf1d2e1acd1&language=es-ES')
+}
 
 
 }
